d29edaeb0a1a398754ac3d36481c2aa7a1f1acea
Uliana Bulygina <ulanabulygina5@gmail.com>
2025-01-22 17:25:31 +0500

    Revert "удалить этот коммит"
    This reverts commit 58402b21c2164903ad40d05567f92457838a1fed.

--- pages/courses/index_course.php
+++ pages/courses/index_course.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="../../CSS/header.css"> <!-- Подключение стилей для шапки -->
     <link rel="stylesheet" href="../../CSS/footer.css"> <!-- Подключение стилей для подвала -->
     <link rel="stylesheet" href="../../CSS/modal_register.css">
-    <link rel="stylesheet" href="../CSS/laboratory.css"> <!-- Подключаем стили для страницы лаборатории -->
+    <link rel="stylesheet" href="../laboratory/laboratory.css"> <!-- Подключаем стили для страницы лаборатории -->
 </head>
 <body>
     <?php include '../../includes/header.php'; ?>
@@ -23,6 +23,5 @@
         
 
     <?php include '../../includes/footer.php'; ?>
-
 </body>
 </html>
